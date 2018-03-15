0dee0e7caa6b8bb532cf7eb115b3ad8190ab7ab8
Jason Jones <jkjmr6@gmail.com>
2018-03-15 10:28:39 -0500

    Add the rest of the config/starter files

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div>You can start me up, I'll never stop. Never, never never...</div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./app/index.js",
+  entry: "./app/index.jsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   module: {
     rules: [
-      {test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
-      {test: /\.(css)$/, use: ['style-loader', 'css-loader']}
-    ]
+      { test: /\.jsx?$/, use: "babel-loader", exclude: /node_modules/ },
+      { test: /\.(css)$/, use: ["style-loader", "css-loader"] },
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'app/index.html'
-  })],
-  mode: "development"
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "app/index.html",
+    }),
+  ],
+  mode: "development",
 };
